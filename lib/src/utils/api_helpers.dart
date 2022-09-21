4dcb630874ce0672196701b661613edb3fcbb336
Kyaw Zayar Tun <kyawzayartun.sbs@gmail.com>
2022-09-21 14:40:11 +0630

    add api messages and http status

--- lib/src/utils/api_helpers.dart
+++ lib/src/utils/api_helpers.dart
@@ -18,7 +18,8 @@ class ApiUtils {
   static const String keyResponseData = 'ResponseData';
 
   static _Status get status => _Status._();
-  static _Message get messages => _Message._();
+  static HttpStatus get httpStatus => HttpStatus._();
+  static ApiMessage get messages => ApiMessage._();
 }
 
 /// ResponseInfo status
@@ -29,13 +30,38 @@ class _Status {
   final String error = '107';
 }
 
+/// Http status
+class HttpStatus {
+  HttpStatus._();
+
+  final int ok = 200;
+  final int created = 201;
+  final int notModified = 304;
+  final int badRequest = 400;
+  final int unAuthorized = 401;
+  final int forbidden = 403;
+  final int notFound = 404;
+  final int tooManyRequests = 429;
+  final int internelServerError = 500;
+  final int serviceUnavailable = 503;
+}
+
 /// APIs messages.
-class _Message {
-  _Message._();
+class ApiMessage {
+  ApiMessage._();
 
   final String unknownError = 'Unknown Error';
   final String connectionProblem = 'Connection Problem';
   final String success = 'Success';
   final String error = 'Error';
   final String failed = 'Failed';
+  final String created = '201 Created';
+  final String notModified = '304 Not modified';
+  final String badRequest = '400 Bad request';
+  final String unAuthorized = '401 Unauthorized';
+  final String forbidden = '403 Forbidden';
+  final String notFound = '404 Not found';
+  final String tooManyRequests = '429 Too many requests';
+  final String internelServerError = '500 Internal server error';
+  final String serviceUnavailable = '503 Service unavailable';
 }
